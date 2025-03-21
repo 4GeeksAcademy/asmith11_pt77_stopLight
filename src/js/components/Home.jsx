e489b0867234d2bd0a7dc724467fc774c0d541ad
aaransmith11 <aaransmith0817@gmail.com>
2025-03-21 23:15:24 +0000

    stopLight complete

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,16 +5,18 @@ export function Home () {
 	const [ selectedColor, setSelectedColor ] = useState("red");
 
 	return (
-		<div className="trafficLight">
-          <div 
-			onClick={() => setSelectedColor("red")} 
-		  className={"light red" + (selectedColor === "red" ? " glow " : "")}></div>
-		  <div 
-		 	onClick={() => setSelectedColor("yellow")}  
-		  className={"light yellow" + (selectedColor === "yellow" ? " glow " : "")}></div>
-		  <div 
-		 	onClick={() => setSelectedColor("green")}  
-		  className={"light green" + (selectedColor === "green" ? " glow " : "")}></div>
+		<div className="div-background">
+			<div className="trafficLight">
+			<div 
+				onClick={() => setSelectedColor("red")} 
+			className={"light red" + (selectedColor === "red" ? " glow " : "")}></div>
+			<div 
+				onClick={() => setSelectedColor("yellow")}  
+			className={"light yellow" + (selectedColor === "yellow" ? " glow " : "")}></div>
+			<div 
+				onClick={() => setSelectedColor("green")}  
+			className={"light green" + (selectedColor === "green" ? " glow " : "")}></div>
+			</div>
 		</div>
 	);
 };
